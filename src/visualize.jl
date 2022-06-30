65cb259d9ad60ad4114a143418a51248f24cc09b
Jinguo Liu <cacate0129@gmail.com>
2022-06-29 22:45:09 -0400

    Jg/refactor4 (#36)
    * update copyline
    
    * modify plot
    
    * update Luxor plot
    
    * fix viz
    
    * new notebook as doc
    
    * rm Manifest
    
    * update tutorial
    
    * fix tutorial
    
    * fix tutorial
    
    * update tutorial

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,11 +1,49 @@
 # normalized to minimum weight and maximum weight
-function LuxorGraphPlot.show_graph(gg::GridGraph; vertex_size=0.35, fontsize=24, kwargs...)
+function LuxorGraphPlot.show_graph(gg::GridGraph;
+        vertex_colors=nothing,
+        vertex_sizes=nothing,
+        vertex_shapes=nothing,
+        vertex_stroke_colors=nothing,
+        vertex_text_colors=nothing,
+        edge_colors=nothing,
+        texts = nothing,
+        format=:png, filename=nothing,
+        vertex_size=0.35,
+        fontsize=24,
+        kwargs...)
+    # transpose (i, j) to make them consistent with terminal output
 	locs = [(j,i) for (i,j) in coordinates(gg)]
-    g, ws = graph_and_weights(gg)
-    show_graph(g; locs, vertex_size, fontsize, kwargs...)
+    length(locs) == 0 && return _draw(()->nothing, 100, 100; format, filename)
+    edges = [(e.src, e.dst) for e in Graphs.edges(graph_and_weights(gg)[1])]
+
+    # configure canvas and plot
+    xmin, ymin, xmax, ymax = LuxorGraphPlot.get_bounding_box(locs)
+    config = LuxorGraphPlot.GraphDisplayConfig(; vertex_size, fontsize, kwargs...)
+    Dx, Dy = ((xmax-xmin)+2*config.xpad)*config.unit, ((ymax-ymin)+2*config.ypad)*config.unit
+    transform(loc) = loc[1]-xmin+config.xpad, loc[2]-ymin+config.ypad
+
+    # compute empty locations
+    empty_locations = Tuple{Int,Int}[]
+    for i=xmin:xmax, j=ymin:ymax
+        (i, j) ∉ locs && push!(empty_locations, (i, j))
+    end
+
+    # plot!
+    LuxorGraphPlot._draw(Dx, Dy; format, filename) do
+        LuxorGraphPlot._show_graph(transform.(locs), edges,
+        vertex_colors, vertex_stroke_colors, vertex_text_colors, vertex_sizes, vertex_shapes, edge_colors, texts, config)
+
+        config2 = LuxorGraphPlot.GraphDisplayConfig(; vertex_size=config.vertex_size/10,
+                                     vertex_fill_color="#333333",
+                                     vertex_stroke_color="transparent",
+                                     kwargs...,
+                                     )
+        LuxorGraphPlot._show_graph(transform.(empty_locations), Tuple{Int,Int}[],
+                nothing, nothing, nothing, nothing, nothing, nothing, fill("", length(empty_locations)), config2)
+    end
 end
 
-function show_grayscale(gg::GridGraph; vertex_size=0.35, fontsize=24, kwargs...)
+function show_grayscale(gg::GridGraph; kwargs...)
 	locs = [(j,i) for (i,j) in coordinates(gg)]
     g, ws = graph_and_weights(gg)
     wmax = maximum(abs, ws)
@@ -14,7 +52,7 @@ function show_grayscale(gg::GridGraph; vertex_size=0.35, fontsize=24, kwargs...)
     # wmax -> 200
     # wmin -> 1
     vertex_colors= [cmap[max(1, round(Int, 100+w/wmax*100))] for w in ws]
-    show_graph(g; locs, vertex_size, fontsize, vertex_colors, kwargs...)
+    show_graph(gg; vertex_colors, kwargs...)
 end
 
 function show_pins(gg::GridGraph, color_pins::AbstractDict; kwargs...)
@@ -64,4 +102,14 @@ end
 function show_config(gg::GridGraph, config; kwargs...)
     vertex_colors=[iszero(c) ? "white" : "red" for c in config]
     show_graph(gg; vertex_colors, kwargs...)
+end
+
+function show_pins(mres::MappingResult; kwargs...)
+    locs = getfield.(mres.grid_graph.nodes, :loc)
+    center_indices = map(loc->findfirst(==(loc), locs), trace_centers(mres))
+    color_pins = Dict{Int,Tuple{String,String}}()
+    for (i, pin) in enumerate(center_indices)
+        color_pins[pin] = ("red", "v$i")
+    end
+    show_pins(mres.grid_graph, color_pins; kwargs...)
 end

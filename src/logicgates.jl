5e0cb0ef00dc6d083078af0fdeb2b101cad9eca3
GiggleLiu <cacate0129@gmail.com>
2022-07-09 04:33:08 +0000

    Deploying to gh-pages from  @ cf6a8a3db618006369e16708dbb1b71002ee4073 🚀

--- src/logicgates.jl
+++ src/logicgates.jl
@@ -1,70 +1,70 @@
+struct Gate{SYM} end
+Gate(x::Symbol) = Gate{x}()
 
-function g_not()
-    u = sqrt(0.5)
-    locs = [(-u, 0.0), (u, 0.0)]
+autosize(locs) = maximum(first, locs), maximum(last, locs)
+function gate_gadget(::Gate{:NOT})
+    locs = [(1, 1), (3, 1)]
     weights = fill(1, 2)
     inputs, outputs = [1], [2]
-    edges = [(1,2)]
-    return locs, edges, weights, inputs, outputs
+    return GridGraph(autosize(locs), Node.(locs, weights), 2.3), inputs, outputs
 end
 
-function g_nor(; rot=3)
-    locs = [rot15(0.0, 1.0, i) for i=rot:rot+4]
+function gate_gadget(::Gate{:NOR})
+    locs = [(2, 1), (1, 3), (2, 5), (4, 2), (4, 4)]
     weights = fill(1, 5)
-    inputs, outputs = [1,3], [2]
-    edges = [(1,2), (2,3), (3,4), (4,5), (5,1)]
-    return locs, edges, weights, inputs, outputs
+    inputs, outputs = [1, 3], [2]
+    return GridGraph(autosize(locs), Node.(locs, weights), 2.3), inputs, outputs
 end
 
-function g_or(; rot=3)
-    locs = [rot15(0.0, 1.0, i) .+ (0.3, 0.0) for i=rot:rot+4]
+function gate_gadget(::Gate{:OR})
+    locs = [(3, 1), (2, 3), (3, 5), (5, 2), (5, 4), (1,3)]
     weights = fill(1, 6)
     weights[2] = 2
     inputs, outputs = [1,3], [6]
-    edges = [(1,2), (2,3), (3,4), (4,5), (5,1), (2,6)]
-    push!(locs, rot15(0.0, 1.0, rot+1) .* 1.75)
-    return locs, edges, weights, inputs, outputs
+    return GridGraph(autosize(locs), Node.(locs, weights), 2.3), inputs, outputs
 end
 
-function g_nxor()
-    locs = [rot16(0.0, 1.0, i) for i=3:8]
-    weights = [1, 2, 1, 2, 1, 2]
-    inputs, outputs = [1,3], [5]
-    edges = [(1,2), (2,3), (3,4), (4,5), (5,6), (1,6), (2,4), (4,6), (2,6)]
-    return locs, edges, weights, inputs, outputs
+function gate_gadget(::Gate{:NXOR})
+    locs = [(2,1), (1,3), (2, 5), (3, 2), (3, 4), (4, 3)]
+    weights = [1, 2, 1, 2, 2, 1]
+    inputs, outputs = [1,3], [6]
+    return GridGraph(autosize(locs), Node.(locs, weights), 2.3), inputs, outputs
 end
 
-function g_xor()
-    locs = [rot16(0.0, 1.0, i) for i=3:8]
-    push!(locs, locs[5] .* 1.75)
+function gate_gadget(::Gate{:XOR})
+    locs = [(2,1), (1,3), (2, 5), (3, 2), (3, 4), (4, 3), (6, 3)]
     weights = [1, 2, 1, 2, 2, 2, 1]
     inputs, outputs = [1,3], [7]
-    edges = [(1,2), (2,3), (3,4), (4,5), (5,6), (1,6), (2,4), (4,6), (2,6), (5,7)]
-    return locs, edges, weights, inputs, outputs
+    return GridGraph(autosize(locs), Node.(locs, weights), 2.3), inputs, outputs
+end
+
+function gate_gadget(::Gate{:AND})
+    u = 1
+    locs = [(-3u, 0), (-u, 0), (0, -u), (0, u), (2u, -u), (2u, u)]
+    locs = map(loc->loc .+ (2u+1, u+1), locs)
+    weights = fill(1, 6)
+    weights[2] = 2
+    inputs, outputs = [1,6], [3]
+    return GridGraph(autosize(locs), Node.(locs, weights), 2.3), inputs, outputs
 end
 
 # inputs are mutually disconnected
 # full adder
 struct VertexScheduler
     count::Base.RefValue{Int}
-    circuit::Vector{Pair{Symbol,Vector{Int}}}
+    circuit::Vector{Pair{Gate,Vector{Int}}}
     edges::Vector{Tuple{Int,Int}}
     weights::Vector{Int}
 end
-VertexScheduler() = VertexScheduler(Ref(0), Pair{Symbol,Vector{Int}}[], Tuple{Int,Int}[], Int[])
+VertexScheduler() = VertexScheduler(Ref(0), Pair{Gate,Vector{Int}}[], Tuple{Int,Int}[], Int[])
 function newvertices!(vs::VertexScheduler, k::Int=1)
     vs.count[] += k
     append!(vs.weights, zeros(Int,k))
     return vs.count[]-k+1:vs.count[]
 end
 
-for (SYM, F) in [
-        (:XOR, :g_xor),
-        (:OR, :g_or),
-        (:AND, :g_and),
-    ]
-@eval function apply!(::Val{$(QuoteNode(SYM))}, vs::VertexScheduler, a::Int, b::Int)
-    locs, edges, weights, inputs, outputs = $F()
+function apply!(g::Gate{SYM}, vs::VertexScheduler, a::Int, b::Int) where SYM
+    locs, edges, weights, inputs, outputs = gadget(g)
     vertices = newvertices!(vs, length(locs)-2)
     out = vertices[end]
 
@@ -83,42 +83,39 @@ for (SYM, F) in [
     vs.weights[mapped_locs] .+= weights
 
     # update circuit
-    push!(vs.circuit, $(QuoteNode(SYM)) => [inputs..., outputs...])
+    push!(vs.circuit, g => [inputs..., outputs...])
 
     return out
 end
-end
 
-function multiplier()
+function logicgate_multiplier()
     c = VertexScheduler()
     x0, x1, x2, x3 = newvertices!(c, 4)
-    x12 = apply!(Val(:AND), c, x1, x2)
-    x4 = apply!(Val(:XOR), c, x0, x12)
-    x5 = apply!(Val(:XOR), c, x3, x4)  # 5 is sum
-    x6 = apply!(Val(:AND), c, x3, x4)
-    x7 = apply!(Val(:AND), c, x0, x12)
-    x8 = apply!(Val(:OR), c, x6, x7)
+    x12 = apply!(Gate(:AND), c, x1, x2)
+    x4 = apply!(Gate(:XOR), c, x0, x12)
+    x5 = apply!(Gate(:XOR), c, x3, x4)  # 5 is sum
+    x6 = apply!(Gate(:AND), c, x3, x4)
+    x7 = apply!(Gate(:AND), c, x0, x12)
+    x8 = apply!(Gate(:OR), c, x6, x7)
     return c, [x0, x1, x2, x3], [x8, x5]
 end
 
-# function and!(c, dx, dy)
-#     locs = [rot16(0.0, 1.0, i) .+ (dx, dy) for i=3:8]
-#     c1 = rgbcolor!(c, 191, 191, 191)
-#     c2 = rgbcolor!(c, 127, 127, 127)
-#     colors = [c1, c2, c1, c2, c1, c2]
-#     texts = ["\$a\$", "", "\$b\$", "", "\$c\$", ""]
-#     edges = [(1,2), (2,3), (3,4), (4,5), (5,6), (1,6), (2,4), (4,6), (2,6)]
-#     vg!(c, locs, edges; colors, texts)
-#     text!(c, dx, dy-1.5, raw"$c = a \veebar b$")
-# end
+truth_table(solver, gate::Gate) = truth_table(solver, gate_gadget(gate)...)
+function truth_table(misenumerator, grid_graph::GridGraph, inputs, outputs)
+    g, ws = graph_and_weights(grid_graph)
+    res = misenumerator(g, ws)
 
-function g_and()
-    u = sqrt(0.5)
-    locs = [(-2u, 0.0), (-u, 0.0), (0.0, -u), (0.0, u), (2u, -u), (2u, u)]
-    weights = fill(1, 6)
-    weights[2] = 2
-    inputs, outputs = [1,6], [3]
-    edges = [(1,2), (2,3), (3,4), (2,4), (3,5), (4,6), (5,6)]
-    return locs, edges, weights, inputs, outputs
+    # create a k-v pair
+    d = Dict{Int,Int}()
+    for config in res
+        input = sum(i->config[inputs[i]]<<(i-1), 1:length(inputs))
+        output = sum(i->config[outputs[i]]<<(i-1), 1:length(outputs))
+        if !haskey(d,input)
+            d[input] = output
+        else
+            @assert d[input] == output
+        end
+    end
+    @assert length(d) == 1<<length(inputs)
+    return [d[i] for i=0:1<<length(inputs)-1]
 end
-

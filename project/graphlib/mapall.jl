14ef305f5cb92c98c099fd20dcb3d79bd5179f5f
GiggleLiu <cacate0129@gmail.com>
2022-07-19 21:39:11 -0400

    new sample graph

--- project/graphlib/mapall.jl
+++ project/graphlib/mapall.jl
@@ -16,24 +16,28 @@ end
 
 @cast function sample(graphname::String; seed::Int=2)
     folder=joinpath(@__DIR__, "data")
+    nrepeat = 100
     if !isdir(folder)
         mkpath(folder)
     end
-    sizes = 10:10:100
+    #sizes = 10:10:100
+    sizes = [10, 14, 24, 38, 62, 100, 158, 250, 398, 630, 1000]
     Random.seed!(seed)
-    res_sizes = Int[]
-    for n in sizes
-        g = if graphname == "Erdos-Renyi"
-            erdos_renyi(n, 0.3)
-        elseif graphname == "3-Regular"
-            random_regular_graph(n, 3)
-        else
-            error("graph name $graphname not defined!")
+    res_sizes = zeros(Int, length(sizes), nrepeat)
+    for (j, n) in enumerate(sizes)
+        for i=1:nrepeat
+            g = if graphname == "Erdos-Renyi"
+                erdos_renyi(n, 0.3)
+            elseif graphname == "3-Regular"
+                random_regular_graph(n, 3)
+            else
+                error("graph name $graphname not defined!")
+            end
+            res = map_graph(g; vertex_order=Greedy(), ruleset=[])
+            m = length(res.grid_graph.nodes)
+            @info "size $n, i = $i, mapped graph size $m."
+            res_sizes, m)
         end
-        res = map_graph(g; vertex_order=Greedy())
-        m = length(res.grid_graph.nodes)
-        @info "size $n, mapped graph size $m."
-        push!(res_sizes, m)
     end
     filename = "$graphname-$seed.dat"
     writedlm(joinpath(folder, filename), res_sizes)
